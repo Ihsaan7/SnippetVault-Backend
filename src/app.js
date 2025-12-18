c73888775169a2ee19d07aa1c790e90185bda0b8
Ihsaan7 <ihsaan2215@gmail.com>
2025-12-18 18:37:15 +0500

    JWT verification is working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import ApiError from "./utils/ApiError.js";
 import AsyncHandler from "./utils/AsyncHandler.js";
 import ApiResponse from "./utils/ApiResponse.js";
@@ -7,6 +8,7 @@ import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import verifyJWT from "../middlewares/auth.mware.js";
 
 const router = express.Router();
+
+router.get("/jwtTest", verifyJWT, (req, res) => {
+  res.json({ message: "You are authenticated", user: req.user });
+});
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
