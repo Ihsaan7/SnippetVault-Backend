26472a412a972b8b557c8225f341a4ddf5ff9642
Ihsaan7 <ihsaan2215@gmail.com>
2025-12-20 12:34:36 +0500

    feat: add Snippet model with schema and compound indexing

---
+++ src/models/Snippet.model.js
@@ -0,0 +1,50 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+
+const SnippetSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required!"],
+      trim: true,
+      index: true,
+    },
+    code: {
+      type: String,
+      required: [true, "Code content is required!"],
+    },
+    language: {
+      type: String,
+      required: true,
+      default: "javascript",
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxLength: 500,
+    },
+    tags: [
+      {
+        type: String,
+        trim: true,
+        lowercase: true,
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+//Compound Indexing
+SnippetSchema.index({ title: "text", tags: "text" });
+
+export const Snippet = mongoose.model("Snippet", SnippetSchema);
