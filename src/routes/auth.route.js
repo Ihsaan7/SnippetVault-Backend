027fba6e67670e36681822af6dbc8b0051a52001
Ihsaan7 <ihsaan2215@gmail.com>
2025-12-23 13:52:28 +0500

    feat: add profile, refresh token, and update routes for account management

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 import {
+  changeAccountDetails,
+  changeCurrentPass,
   loginUser,
   logoutUser,
+  profileUser,
+  refreshAccessToken,
   registerUser,
+  updateAvatar,
 } from "../controllers/auth.controller.js";
 import verifyJWT from "../middlewares/auth.mware.js";
 import { upload } from "../middlewares/multer.mware.js";
@@ -20,5 +25,20 @@ router.post(
 );
 router.post("/login", loginUser);
 router.post("/logout", verifyJWT, logoutUser);
+
+router.get("/profile", verifyJWT, profileUser);
+
+router.post("/refresh", refreshAccessToken);
+
+router.patch("/update-profile", verifyJWT, changeAccountDetails);
+
+router.patch("/update-password", verifyJWT, changeCurrentPass);
+
+router.patch(
+  "/update-avatar",
+  verifyJWT,
+  upload.fields([{ name: "avatar", maxCount: 1 }]),
+  updateAvatar
+);
 
 export default router;
